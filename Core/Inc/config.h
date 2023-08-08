4f20a9744806c72ca2fe2d24c80e5fbc83fa9a50
northgoblin <141734045+northgoblin@users.noreply.github.com>
2023-08-08 16:40:07 +0200

    Update config.h

--- Core/Inc/config.h
+++ Core/Inc/config.h
@@ -4,15 +4,15 @@
 #define BRAKEMAX 100
 
 // speed limits for invividual modes in kph
-#define SPEEDLIMIT_ECO 6
-#define SPEEDLIMIT_NORMAL 20
-#define SPEEDLIMIT_SPORT 50
+#define SPEEDLIMIT_ECO 15
+#define SPEEDLIMIT_NORMAL 25
+#define SPEEDLIMIT_SPORT 30
 
 // motor current limits for invividual modes in mA
 // note that hacked firmware allows up to 55amps motor phase current
-#define PH_CURRENT_MAX_ECO 10000
-#define PH_CURRENT_MAX_NORMAL 15000
-#define PH_CURRENT_MAX_SPORT 25000
+#define PH_CURRENT_MAX_ECO 15000
+#define PH_CURRENT_MAX_NORMAL 25000
+#define PH_CURRENT_MAX_SPORT 35000
 
 // motor current limit for regen in mA
 #define REGEN_MAX_CURRENT 10000
